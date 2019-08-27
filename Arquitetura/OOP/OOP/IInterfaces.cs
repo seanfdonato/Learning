9b1b6cd02760193c277ca27cb7483dc9c5d188f7
seanfdonato <seafdonato@gmail.com>
2019-08-27 15:25:12 -0300

    SOLID

---
+++ Arquitetura/OOP/OOP/IInterfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public interface IInterfaces
+    {
+        void Test();
+        int TestInt(int y);
+    }
+
+    class ExtendsInterfaces : IInterfaces
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TestInt(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
