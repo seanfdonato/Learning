eb209d11e1d65ba15c16ff53c98fb71d041b3704
seanfdonato <39416945+seanfdonato@users.noreply.github.com>
2020-02-16 21:16:13 +0000

    rxjs

---
+++ Rxjs/reactive-patterns-course-master/src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {fromEvent, Subscription} from 'rxjs';
+
+@Component({
+  selector: 'browser-event-experiments',
+  templateUrl: './browser-event-experiments.component.html',
+  styleUrls: ['./browser-event-experiments.component.css']
+})
+export class BrowserEventExperimentsComponent implements OnInit {
+
+    hoverSection: HTMLElement;
+
+    sub: Subscription;
+
+  ngOnInit() {
+
+      this.hoverSection = document.getElementById('hover');
+
+      const mouseMove$ = fromEvent(this.hoverSection, 'mousemove');
+
+      const click$ = fromEvent(this.hoverSection, 'click');
+
+      const combined$  = click$.withLatestFrom(mouseMove$)
+            .map(events => events[1]);
+
+      combined$.subscribe(console.log);
+
+  }
+
+    unsubscribe() {
+      console.log('Called unsubscribe()');
+      this.sub.unsubscribe();
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
